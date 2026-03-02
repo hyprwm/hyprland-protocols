2e1e53e103cd9ec79741dee6e9627e6ffa79cc88
Mihai Fufezan <mihai@fufexan.net>
2026-03-02 16:00:19 +0200

    treewide: alejandra -> nixfmt

--- flake.nix
+++ flake.nix
@@ -7,40 +7,52 @@
     systems.url = "github:nix-systems/default-linux";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    systems,
-    ...
-  }: let
-    inherit (nixpkgs) lib;
-    eachSystem = lib.genAttrs (import systems);
-    pkgsFor = eachSystem (system:
-      import nixpkgs {
-        localSystem = system;
-        overlays = [self.overlays.hyprland-protocols];
-      });
-    mkDate = longDate: (lib.concatStringsSep "-" [
-      (builtins.substring 0 4 longDate)
-      (builtins.substring 4 2 longDate)
-      (builtins.substring 6 2 longDate)
-    ]);
-    version = lib.removeSuffix "\n" (builtins.readFile ./VERSION);
-  in {
-    overlays = {
-      default = self.overlays.hyprland-protocols;
-      hyprland-protocols = final: prev: {
-        hyprland-protocols = final.callPackage ./nix/default.nix {
-          version = version + "+date=" + (mkDate (self.lastModifiedDate or "19700101")) + "_" + (self.shortRev or "dirty");
+  outputs =
+    {
+      self,
+      nixpkgs,
+      systems,
+      ...
+    }:
+    let
+      inherit (nixpkgs) lib;
+      eachSystem = lib.genAttrs (import systems);
+      pkgsFor = eachSystem (
+        system:
+        import nixpkgs {
+          localSystem = system;
+          overlays = [ self.overlays.hyprland-protocols ];
+        }
+      );
+      mkDate =
+        longDate:
+        (lib.concatStringsSep "-" [
+          (builtins.substring 0 4 longDate)
+          (builtins.substring 4 2 longDate)
+          (builtins.substring 6 2 longDate)
+        ]);
+      version = lib.removeSuffix "\n" (builtins.readFile ./VERSION);
+    in
+    {
+      overlays = {
+        default = self.overlays.hyprland-protocols;
+        hyprland-protocols = final: prev: {
+          hyprland-protocols = final.callPackage ./nix/default.nix {
+            version =
+              version
+              + "+date="
+              + (mkDate (self.lastModifiedDate or "19700101"))
+              + "_"
+              + (self.shortRev or "dirty");
+          };
         };
       };
+
+      packages = eachSystem (system: {
+        inherit (pkgsFor.${system}) hyprland-protocols;
+        default = self.packages.${system}.hyprland-protocols;
+      });
+
+      formatter = eachSystem (system: nixpkgs.legacyPackages.${system}.nixfmt-tree);
     };
-
-    packages = eachSystem (system: {
-      inherit (pkgsFor.${system}) hyprland-protocols;
-      default = self.packages.${system}.hyprland-protocols;
-    });
-
-    formatter = eachSystem (system: nixpkgs.legacyPackages.${system}.alejandra);
-  };
 }
--- nix/default.nix
+++ nix/default.nix
@@ -10,7 +10,7 @@ stdenv.mkDerivation {
 
   src = ../.;
 
-  nativeBuildInputs = [cmake];
+  nativeBuildInputs = [ cmake ];
 
   meta = {
     homepage = "https://github.com/hyprwm/hyprland-protocols";
